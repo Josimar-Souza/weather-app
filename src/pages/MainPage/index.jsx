eacd7b1f0bbd0880b49de9f70f33b1df3429c752
Josimar Souza <contact.josimarsouza@gmail.com>
2023-06-13 13:37:18 -0300

    feat: Added search section

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
-import MainPageContainer from './mainPageStyles';
+import { Divider } from 'antd';
+import {
+  MainPageContainer,
+  SearchContainer,
+  SearchTitle,
+  CustomInput,
+  CustomDividerContainer,
+  CustomDividerLine,
+  CustomDividerInfo,
+  CustomButton,
+} from './mainPageStyles';
 
 function MainPage() {
   return (
-    <MainPageContainer />
+    <MainPageContainer>
+      <SearchContainer>
+        <SearchTitle>Weather App</SearchTitle>
+        <Divider />
+        <CustomInput placeholder="Enter a city name" />
+        <CustomDividerContainer>
+          <CustomDividerLine />
+          <CustomDividerInfo>or</CustomDividerInfo>
+          <CustomDividerLine />
+        </CustomDividerContainer>
+        <CustomButton>Get device location</CustomButton>
+      </SearchContainer>
+    </MainPageContainer>
   );
 }
 
